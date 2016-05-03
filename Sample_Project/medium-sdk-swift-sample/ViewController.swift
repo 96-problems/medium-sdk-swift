0d066b693e16ec2cd62097da4b32120415ad15a0
drinkius <telegin.alexander@gmail.com>
2016-05-03 16:11:36 +0300

    first commit

---
+++ Sample_Project/medium-sdk-swift-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  medium-sdk-swift-sample
+//
+//  Created by drinkius on 02/05/16.
+//  Copyright © 2016 96problems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
